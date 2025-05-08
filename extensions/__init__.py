307e1b65f4a9a26f3e1976ba508ca69c4ad82b75
David Hensle <hensle93@gmail.com>
2025-05-08 11:35:07 -0700

    removed transponder ownership, cleaned up write matrices

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,5 +1,4 @@
 from . import av_ownership
 from . import external_identification
 from . import external_location_choice
-from . import transponder_ownership
 from . import adjust_auto_operating_cost
