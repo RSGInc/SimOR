daa8819bab960e3b7a3cd1b24aa7a62f3bc44015
David Hensle <hensle93@gmail.com>
2025-05-08 16:36:01 -0700

    calling python directly instead of coverage

--- test/test_cropped_dataset.py
+++ test/test_cropped_dataset.py
@@ -1,16 +1,10 @@
 import subprocess
-import sys
-import os
 
 def test_cropped_example():
-    file_path = os.path.join(os.path.dirname(__file__), "../simulation.py")
 
     subprocess.run(
         [
-            "coverage",  # Command to run coverage
-            "run",  # Command to run the script
-            "-a",  # Append coverage data
-            file_path,  # Path to the script
+            "python", "simulation.py",
             "-c", "configs",  # Config directory
             "-d", "model_data/cropped_data",  # Input data directory
             "-o", "outputs/test"  # Output directory
