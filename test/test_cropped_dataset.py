8fbf839fe9f37cc0f573e52c25e4b6bcb780e55e
David Hensle <51132108+dhensle@users.noreply.github.com>
2025-07-18 15:18:51 -0700

    CI Setup (#3)
    * refresh ci test for metro cropped data
    
    * adding coverage install
    
    * trying ci test with conda env install from asim clone
    
    * miniforge to miniconda for github actions setup
    
    * removing mamba references
    
    * quotes around python version
    
    * fixing bad activitysim pip install
    
    * trying with matrix and python versioning
    
    * misspelled conda env file name
    
    * install asim from base dir
    
    * trying to run from inside asim install location
    
    * installing activitysim not inside SimOR
    
    * printing directory structure
    
    * trying to get paths right
    
    * full path for environment file
    
    * relative path for conda env yaml
    
    * more paths

--- test/test_cropped_dataset.py
+++ test/test_cropped_dataset.py
@@ -1,16 +1,21 @@
 import subprocess
+import os
+import sys
 
 def test_cropped_example():
 
-    subprocess.run(
-        [
-            "python", "simulation.py",
-            "-c", "configs",  # Config directory
-            "-d", "model_data/cropped_data",  # Input data directory
-            "-o", "outputs/test"  # Output directory
-        ], 
-        check=True
-    )
+    file_path = os.path.join(os.path.dirname(__file__), "../simulation.py")
+
+    run_args = [
+        "-c", "configs",  # Config directory
+        "-d", "model_data/metro_data_cropped",  # Input data directory
+        "-o", "outputs/test"  # Output directory
+    ]
+
+    if os.environ.get("GITHUB_ACTIONS") == "true":
+        subprocess.run(["coverage", "run", "-a", file_path] + run_args, check=True)
+    else:
+        subprocess.run([sys.executable, file_path] + run_args, check=True)
     
     return True
 
